f7a5e64db8c91992828325b632c51af4ebd3a022
notfreshmbp <1195056983@qq.com>
2019-06-04 13:44:17 +0800

    problem160

---
+++ template.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# date: 2019-06-04
+# problem-url:
+#
+
+# problem description:
+#
+
+class Solution:
+    def f1(self, s):
+        pass
+
+if __name__ == '__main__':
+    s = Solution()
+    test1 = ''
+    res = s.f1(test1)
+    print(res)
